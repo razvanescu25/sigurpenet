a96636bb41106e1ab8e8798a04895737cd595a2e
razvanescu25 <razvanescu25@users.noreply.github.com>
2024-11-28 19:07:50 +0200

    fist deployment

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,8 +1,9 @@
 // List of categories for blog posts
 export const CATEGORIES = [
-	'Category 1',
-	'Category 2',
-	'Category 3',
-	'Category 4',
-	'Category 5'
+	'Social Media',
+	'Utilizare Software',
+	'Protectie Date',
+	'Tentative Fraude',
+	'Fake News',
+	'Continut AI'
 ] as const
--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -12,10 +12,10 @@ interface SiteConfig {
 export const siteConfig: SiteConfig = {
 	site: 'https://blog-template-gray.vercel.app/', // Write here your website url
 	author: 'Your Name', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
+	title: 'Sigur Pe Net', // Site title.
+	description: 'The place where we discuss internet safety and best practices to stay tech-healthy', // Description to display in the meta tags
+	lang: 'ro-RO',
+	ogLocale: 'ro_RO',
 	shareMessage: 'Share this post', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
 }
